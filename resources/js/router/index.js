9a3926913f681a19d1dfe9b81a4006b5004a856d
gollumeo <p.mauriello54@gmail.com>
2024-03-20 20:59:35 +0100

    Init vue-router + routes definition

---
+++ resources/js/router/index.js
@@ -0,0 +1,10 @@
+import {createRouter} from "vue-router";
+import {createWebHistory} from "vue-router";
+
+const routes = [
+    ]
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
