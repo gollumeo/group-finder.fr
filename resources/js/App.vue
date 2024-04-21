34172efd12f67717639da109e3c5477cd97559e7
gollumeo <p.mauriello54@gmail.com>
2024-04-21 23:25:21 +0200

    Landing page

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -74,18 +74,7 @@
 export default {
     name: 'App.vue',
     mounted() {
-        console.log('App.vue component mounted.');
 
-        // Catch errors from the API using interceptor thing, so we can reject them and treat them in the components
-        this.$instance.interceptors.response.use(
-            (response) => {
-                return response;
-            },
-            (error) => {
-                console.log('Error:', error.response.data);
-                return Promise.reject(error);
-            }
-        );
     }
 }
 </script>
