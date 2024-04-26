97e048a85b635fc3ea16736324b9d8a161ebe550
gollumeo <p.mauriello54@gmail.com>
2024-04-26 23:42:05 +0200

    FIX - bg img size Added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,7 @@
         href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet"
     />
+    <link rel="shortcut icon" href="{{asset('./assets/img/logo.jpeg')}}" type="image/x-icon">
     <title>{{config('app.name')}}</title>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
