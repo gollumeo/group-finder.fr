0c165ef0106f62dc3208e994e481cf632298ce0d
gollumeo <p.mauriello54@gmail.com>
2024-06-16 16:23:34 +0200

    test btn

--- resources/js/views/lab/Laboratory.vue
+++ resources/js/views/lab/Laboratory.vue
@@ -1,5 +1,7 @@
 <template>
-<h1>coucou</h1>
+    <div class="p-4 border border-indigo-100 h-full">
+        <button class="inline-flex px-3 py-2 bg-indigo-50 text-indigo-800 rounded-[5px] font-medium hover:bg-indigo-200">Button test</button>
+    </div>
 </template>
 
 <script>
