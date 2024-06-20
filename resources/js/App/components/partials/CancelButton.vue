778bb39256de5de4699149da40bf817a7d2d7c5b
gollumeo <p.mauriello54@gmail.com>
2024-06-20 22:46:21 +0200

    Cancel button

--- resources/js/App/components/partials/CancelButton.vue
+++ resources/js/App/components/partials/CancelButton.vue
@@ -1,9 +1,53 @@
 <template>
-  $END$
+    <button :class="buttonClass" @click="emitCancel" :disabled="disabled">
+        <font-awesome-icon v-if="icon" :icon="icon" />
+        {{ innerText }}
+    </button>
 </template>
 
 <script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+
 export default {
-name: "CancelButton"
+    name: "CancelButton",
+    components: {FontAwesomeIcon},
+    emits: ['cancel'],
+
+    props: {
+        innerText: {
+            type: String,
+            default: 'Annuler'
+        },
+        icon: {
+            type: String,
+            default: 'fa-solid fa-check'
+        },
+        size: {
+            type: String,
+            validator(value) {
+                return ['sm', 'base'].includes(value)
+            },
+            default: 'base'
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    computed: {
+        buttonClass() {
+            return {
+                'btn btn-red btn-sm': this.size === 'sm',
+                'btn btn-red': this.size === 'base'
+            }
+        }
+    },
+
+    methods: {
+        emitCancel() {
+            this.$emit('cancel');
+        }
+    }
 }
 </script>
--- resources/js/App/views/lab/Laboratory.vue
+++ resources/js/App/views/lab/Laboratory.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="p-4 border border-indigo-100 h-full flex gap-2">
-        <button class="btn">
-            <font-awesome-icon icon="fa-solid fa-check" />
-            Button test
-        </button>
-        <ValidateButton innerText="test" @validate="console.log('coucou')" disabled/>
+        <ValidateButton />
+        <ValidateButton innerText="Disabled" @validate="console.log('coucou')" disabled/>
+        <CancelButton @cancel="console.log('coucou')" />
+        <CancelButton innerText="Disabled" disabled/>
     </div>
 </template>
 
 <script>
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import ValidateButton from "@partials/ValidateButton.vue";
+import CancelButton from "@partials/CancelButton.vue";
 
 export default {
     name: "Laboratory",
-    components: {ValidateButton, FontAwesomeIcon}
+    components: {CancelButton, ValidateButton, FontAwesomeIcon}
 }
 </script>
