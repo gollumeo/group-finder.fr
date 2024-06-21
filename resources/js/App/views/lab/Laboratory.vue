63126f2a089a50676117e95de2eadc37a023d837
gollumeo <p.mauriello54@gmail.com>
2024-06-22 00:12:26 +0200

    save button is now greenish (teal)

--- resources/js/App/views/lab/Laboratory.vue
+++ resources/js/App/views/lab/Laboratory.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="p-4 border border-indigo-100 h-full flex gap-2">
+    <div class="p-4 border border-indigo-100 h-full flex gap-2 items-center">
         <ValidateButton />
-        <ValidateButton innerText="Disabled" @validate="console.log('coucou')" disabled/>
+        <ValidateButton innerText="Disabled" @validate="console.log('coucou')" disabled size="sm"/>
         <CancelButton @cancel="console.log('coucou')" />
-        <CancelButton disabled/>
+        <CancelButton disabled size="sm"/>
+        <SaveButton />
+        <SaveButton disabled size="sm" />
     </div>
 </template>
 
@@ -11,9 +13,10 @@
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import ValidateButton from "@partials/ValidateButton.vue";
 import CancelButton from "@partials/CancelButton.vue";
+import SaveButton from "@partials/SaveButton.vue";
 
 export default {
     name: "Laboratory",
-    components: {CancelButton, ValidateButton, FontAwesomeIcon}
+    components: {SaveButton, CancelButton, ValidateButton, FontAwesomeIcon}
 }
 </script>
